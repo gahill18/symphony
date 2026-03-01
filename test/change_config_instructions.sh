9e18610283b8d367dc3f9471ca0102f0a7443e0c
Garrett Hill <gahill2018@protonmail.com>
2026-03-01 14:28:48 -0500

    fixing string characters in echo instruction

--- test/change_config_instructions.sh
+++ test/change_config_instructions.sh
@@ -1,2 +1,2 @@
 cat ./test/config.json > ./test/old_config.json
-echo "{ \"source_url\": \"https://github.com/gahill18/symphony/raw/refs/heads/feature-get-instructions/test/instructions.sh\", \"time_to_wait\": 1 }" > ./test/config.json
+echo '{ "source_url": "https://github.com/gahill18/symphony/raw/refs/heads/feature-get-instructions/test/instructions.sh", "time_to_wait": 1 }' > ./test/config.json
