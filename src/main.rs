24eff1a69b6a34e2b7e1a5dcca60c103cf534023
Garrett Hill <gahill2018@protonmail.com>
2026-03-05 09:46:41 -0500

    updated Script obj to store commands as a vector of lines and store the outputs from the most recent time it was executed. Also updated config json files to reference main branch instead of feature branches

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::fs;
 use std::path::PathBuf;
-use std::process::Command;
+use std::process::{Command, Output};
 
 use clap::Parser;
 use serde::{Deserialize, Serialize};
@@ -27,54 +27,61 @@ impl Config {
 
 #[derive(Debug)]
 struct Script {
-    cmds: Vec<Command>,
+    cmds: Vec<String>,
+    last_outputs: Vec<Output>,
 }
 
 impl Script {
-    /* Use a Command to build a Script object from a list of strings */
+    /* Build a Script object from a list of strings */
     fn from_lines(lines: Vec<&str>) -> Script {
-        let mut cmds: Vec<Command> = Vec::new();
-        for l in lines {
-            // TODO: Support non-sh scripts
-            // TODO: Async Child Process Spawn
-            // TODO: Push Command obj to cmds
-            cmds.push(Command::new("sh"));
-            let output = Command::new("sh")
-                .arg("-c")
-                .arg(l)
-                .output()
-                .expect("failed");
-            println!("Command Output of {}: {:?}", l, output);
-            // cmds.push(cmd);
-        }
-        return Script { cmds };
+        let cmds: Vec<String> = lines.iter().map(|&l| String::from(l)).collect();
+        let last_outputs: Vec<Output> = Vec::new();
+        return Script { cmds, last_outputs };
     }
 
-    /*
-    Use HTTP Get to build a Script object from a source URL argument
-    */
+    /* Use HTTP Get to build a Script object from a source URL argument */
     fn from_source_url(src_url: String) -> Script {
         let response = reqwest::blocking::get(src_url).unwrap();
         let text: String = if response.status() != 200 {
-            String::new()
+            panic!("Failed to get a proper response");
         } else {
             response.text().unwrap()
         };
         let lines: Vec<&str> = text.lines().collect();
         Script::from_lines(lines)
     }
+
+    fn execute(&mut self) -> Result<(), Box<dyn std::error::Error>> {
+        let mut outputs: Vec<Output> = Vec::new();
+        for c in self.cmds.iter() {
+            // TODO: Support non-sh scripts
+            // TODO: Async Child Process Spawn
+            let output: Output = Command::new("sh")
+                .arg("-c")
+                .arg(c)
+                .output()
+                .expect("failed");
+            outputs.push(output);
+        }
+        self.last_outputs = outputs;
+        return Ok(());
+    }
 }
 
 fn main() {
     let args: Args = Args::parse();
     let conf_path_string: String = args.config_path.unwrap();
+    let mut loop_continue: bool = true;
 
-    while true {
+    while loop_continue {
         let deserialized_config: Config = Config::from_path_string(String::from(&conf_path_string));
-        let script: Script = Script::from_source_url(String::from(&deserialized_config.source_url));
+        let mut script: Script =
+            Script::from_source_url(String::from(&deserialized_config.source_url));
         std::thread::sleep(std::time::Duration::from_secs(
             deserialized_config.time_to_wait,
         ));
+        loop_continue = script.execute().is_ok();
+        println!("{:?}", script);
     }
 }
 
